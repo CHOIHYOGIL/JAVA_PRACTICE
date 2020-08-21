54f045b7f03656e4035b07703bb286ac5a030ab7
CHOIHYOGIL <gyrlf2478@naver.com>
2020-08-21 18:12:35 +0900

    Test

---
+++ HelloJavaProject/src/kr/or/iei/HelloJava.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("æ»≥Á«œººø‰1");  //æ»≥Á«œººø‰1
+		System.out.println("æ»≥Á«œººø‰2"); //æ»≥Á«œººø‰2
+		
+
+	}
+
+}
