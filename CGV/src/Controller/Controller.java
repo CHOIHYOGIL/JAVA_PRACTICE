a42dc907bf0a7af1b12296dd713b704e7840c5c2
CHOIHYOGIL <gyrlf2478@naver.com>
2020-09-09 23:05:46 +0900

    영화 추가 수정 삭제 , 상영중인 영화 재고 현황, 상품 등록, 수정 ,삭제 완료

--- CGV/src/Controller/Controller.java
+++ CGV/src/Controller/Controller.java
@@ -1,22 +1,54 @@
 package Controller;
 
 import View.CgvView;
+import model.Drink;
+import model.Food;
 import model.Movie;
+import model.Popcorn;
+import model.Product;
 
 public class Controller {
 	
 	CgvView view;
 	
 	Movie [] movie;
-	int index=0;
+	int mindex=0;
+	
+	Product [] product=new Product[10];
+	int pIndex=0;
+	
+	Drink[] drink=new Drink[10];
+	int dIndex=0;
+	
+	Food[] food=new Food[10];
+	int fIndex=0;
+	
+	Popcorn[] popcorn=new Popcorn[10];
+	int popIndex=0;
+	
 		public Controller() {
 			movie=new Movie[10];
 			view=new CgvView();
+			drink[dIndex++] = new Drink("Æé½Ã   ", 30, 2500);
+			drink[dIndex++] = new Drink("»çÀÌ´Ù ", 20, 2500);
+			drink[dIndex++] = new Drink("È¯Å¸   ", 25, 3000);
+			drink[dIndex++] = new Drink("ÁÖ½º", 40, 2000);
+			drink[dIndex++] = new Drink("³ÃÄ¿ÇÇ ", 10, 2000);
+			food[fIndex++] = new Food("³ªÃÝ   ", 10, 3000);
+			food[fIndex++] = new Food("ÇÁ·¹Áñ ", 20, 2000);
+			food[fIndex++] = new Food("ÇÖµµ±× ", 17, 4000);
+			food[fIndex++] = new Food("ÇÖ¹Ù   ", 18, 4000);
+			food[fIndex++] = new Food("¿ÀÂ¡¾î ", 19, 4500);
+			popcorn[popIndex++] = new Popcorn("Ä«·ª¸áÆËÄÜ", 200, 5000, "´ÞÄÞÇÑ¸À");
+			popcorn[popIndex++] = new Popcorn("ÆËÄÜ      ", 200, 5000, "Â¬Â©ÇÑ¸À");
+			popcorn[popIndex++] = new Popcorn("Ä¡ÁîÆËÄÜ  ", 200, 5000, "Ä¡Áî¸À");
+
+			
 		}
 	
 	public void main() {
-		
-		while(true) {
+		boolean bool=true;
+		while(bool) {
 			int sel=view.Welcome();
 			
 			switch(sel) {
@@ -27,7 +59,9 @@ public class Controller {
 				break;
 			case 2:
 				break;
-			case 0:
+			case 3:
+				bool=false;
+				System.out.println("Á¾·áÇÕ´Ï´Ù.");
 				break;
 			default:
 				System.out.println("Àß¸øÀÔ·ÂÇß½À´Ï´Ù");
@@ -52,16 +86,16 @@ public class Controller {
 			ShowMovie();
 			break;
 		case 5:
-			//ShowThings();
+			ShowThings();
 			break;
 		case 6:
-			//insertThing();
+			insertThing();
 			break;
 		case 7:
-			//UpdateThing();
+			UpdateThing();
 			break;
 		case 8:
-			//DeleteThing();
+			DeleteThings();
 			break;
 		case 0:
 			main();
@@ -69,31 +103,103 @@ public class Controller {
 		}
 		
 	}
+	
+	
+	public void DeleteThings() {
+		int sel=view.DeleteThings();
+		switch(sel) {
+		case 1:
+			int choice=view.deleteDrink(dIndex,drink);
+			for(int i=choice; i<dIndex; i++) {
+				drink[i]=drink[i+1];
+			}
+			dIndex--;
+			break;
+		case 2:
+			int choice1=view.deleteFood(fIndex,food);
+			for(int i=choice1; i<fIndex; i++) {
+				food[i]=food[i+1];
+			}
+			fIndex--;
+			break;
+		case 3:
+			int choice3=view.deletePopcorn(popIndex,popcorn);
+			for(int i=choice3; i<popIndex; i++) {
+				popcorn[i]=popcorn[i+1];
+			}
+			popIndex--;
+			break;
+		default:
+			System.out.println("Àß¸øÀÔ·Â");
+			break;
+		}
+	}
+	public void UpdateThing() {
+		int sel=view.UpdateThing();
+		switch(sel) {
+		case 1:
+			view.updateDrink(dIndex,drink);
+			break;
+		case 2:
+			view.updateFood(fIndex,food);
+			break;
+		case 3:
+			view.updatePopcorn(popIndex,popcorn);
+			break;
+		default:
+			System.out.println("Àß¸øÀÔ·Â");
+			break;
+		}
+	}
+	public void insertThing() {
+		
+		int sel=view.insertThing();
+		switch(sel) {
+		case 1:
+			Drink d=view.insertDrink();
+			drink[dIndex++]=d;
+			break;
+		case 2:
+			Food f=view.insertFood();
+			food[fIndex++]=f;
+			break;
+		case 3:
+			Popcorn p=view.insertPopcorn();
+			popcorn[popIndex++]=p;
+			break;
+		}
+	}
+	public void ShowThings() {
+		
+		view.showDrink(dIndex, drink);
+		view.showFood(fIndex, food);
+		view.showPopcorn(popIndex, popcorn);
+	}
 	public	void ShowMovie() {
 		
-		view.ShowMovie(index,movie);
+		view.ShowMovie(mindex,movie);
 	}
 	public void insertMovie() {
 		
 		Movie m=view.InsertMovie();
-		movie[index++] =m;
+		movie[mindex++] =m;
 		view.Alba();
 	}
 	
 	public void UpdateMovie() {
 		
-		view.UpdateMovie(index,movie);
+		view.UpdateMovie(mindex,movie);
 		
 	}
 	
 	public void DeleteMovie() {
-		String name=view.DeleteMovie(index, movie);
+		String name=view.DeleteMovie(mindex, movie);
 		int search=searchIndex(name);
 		
-		for(int i=search; i<index-1; i++) {
+		for(int i=search; i<mindex-1; i++) {
 			movie[i]=movie[i+1];
 		}
-		index--;
+		mindex--;
 		
 	}
 	public int searchIndex(String name) {
--- CGV/src/View/CgvView.java
+++ CGV/src/View/CgvView.java
@@ -2,7 +2,10 @@ package View;
 
 import java.util.Scanner;
 
+import model.Drink;
+import model.Food;
 import model.Movie;
+import model.Popcorn;
 
 public class CgvView {
 	Scanner scanner;
@@ -183,4 +186,194 @@ public class CgvView {
 		
 
 	}
+	
+	// 음료 보기
+		public void showDrink(int dIndex, Drink []drink1) {
+			System.out.println("\n───────── 음료 ─────────");
+			System.out.println("No.\t\t제품명\t\t수량\t\t가격");
+			for (int i = 0; i < dIndex; i++) {
+				System.out
+						.println((i + 1) + "\t\t" + drink1[i].getName() + "\t\t" + drink1[i].getAmount() + "\t\t" + drink1[i].getPrice());
+			}
+		}
+
+		// 음식 보기
+		public void showFood(int fIndex, Food []food) {
+			System.out.println("\n───────── 음식 ─────────");
+			System.out.println("No.\t\t제품명\t\t수량\t\t가격");
+			for (int i = 0; i < fIndex; i++) {
+				System.out
+						.println((i + 1) + "\t\t" + food[i].getName() + "\t\t" + food[i].getAmount() + "\t\t" + food[i].getPrice());
+			}
+		}
+
+		// 팝콘 보기
+		public void showPopcorn(int popIndex, Popcorn []popcorn) {
+			System.out.println("\n───────── 팝콘 ─────────");
+			System.out.println("No.\t\t제품명\t\t\t\t수량\t\t가격\t\t맛");
+			for (int i = 0; i < popIndex; i++) {
+				System.out.println((i + 1) + "\t\t" + popcorn[i].getName() + "\t\t\t" + popcorn[i].getAmount() + "\t\t"
+						+ popcorn[i].getPrice() + "\t\t" + popcorn[i].getTaste());
+			}
+		}
+		public Drink insertDrink() {
+			System.out.print("제품명 입력 : ");
+			String drinkName=scanner.next();
+			System.out.print("수량 입력 : ");
+			int drinkAmount=scanner.nextInt();
+			System.out.print("가격 입력 : ");
+			int drinkPrice=scanner.nextInt();
+			Drink drink1=new Drink(drinkName, drinkAmount, drinkPrice);
+			System.out.println("등록완료");
+			return drink1;
+			
+		}
+		public Food insertFood() {
+			System.out.print("제품명 입력 : ");
+			String foodName=scanner.next();
+			System.out.print("수량 입력 : ");
+			int foodAmount=scanner.nextInt();
+			System.out.print("가격 입력 : ");
+			int foodPrice=scanner.nextInt();
+			Food food1=new Food(foodName, foodAmount, foodPrice);
+			System.out.println("등록완료");
+			return food1;
+		}
+		public Popcorn insertPopcorn() {
+			System.out.print("제품명 입력 : ");
+			String PopcornName=scanner.next();
+			System.out.print("수량 입력 : ");
+			int PopcornAmount=scanner.nextInt();
+			System.out.print("가격 입력 : ");
+			int PopcornPrice=scanner.nextInt();
+			System.out.print("맛 입력 : ");
+			String taste=scanner.next();
+			Popcorn pop1=new Popcorn(PopcornName, PopcornAmount, PopcornPrice,taste);
+			System.out.println("등록완료");
+			return pop1;
+			
+		}
+		public void updateDrink(int dIndex, Drink[]drink1) {
+			System.out.println("\n───────── 음료 ─────────");
+			System.out.println("No.\t\t제품명\t\t수량\t\t가격");
+			for (int i = 0; i < dIndex; i++) {
+				System.out
+						.println((i + 1) + "\t\t" + drink1[i].getName() + "\t\t" + drink1[i].getAmount() + "\t\t" + drink1[i].getPrice());
+			}
+			
+			System.out.print("재고를 변경할 제품의 No.를 선택해주세요 >");
+			int sel=scanner.nextInt();
+			System.out.println(drink1[sel-1].getName()+"제품을 선택하셨습니다.");
+			System.out.println("현재 재고는"+drink1[sel-1].getAmount()+"입니다.");
+			System.out.print("재고를 몇 개로 수정할지 입력해주세요 > ");
+			int drinkA=scanner.nextInt();
+			drink1[sel-1].setAmount(drinkA);
+			System.out.println(drinkA+"개로 수정 완료 되었습니다.");
+		}
+		public void updateFood(int fIndex, Food[]food) {
+			System.out.println("\n───────── 음식 ─────────");
+			System.out.println("No.\t\t제품명\t\t수량\t\t가격");
+			for (int i = 0; i < fIndex; i++) {
+				System.out
+						.println((i + 1) + "\t\t" + food[i].getName() + "\t\t" + food[i].getAmount() + "\t\t" + food[i].getPrice());
+			}
+			System.out.print("재고를 변경할 제품의 No.를 선택해주세요 >");
+			int sel=scanner.nextInt();
+			System.out.println(food[sel-1].getName()+"제품을 선택하셨습니다.");
+			System.out.println("현재 재고는"+food[sel-1].getAmount()+"입니다.");
+			System.out.print("재고를 몇 개로 수정할지 입력해주세요 > ");
+			int foodA=scanner.nextInt();
+			food[sel-1].setAmount(foodA);
+			System.out.println(foodA+"개로 수정 완료 되었습니다.");
+		}
+		public void updatePopcorn(int popIndex, Popcorn[]popcorn) {
+		
+			System.out.println("\n───────── 팝콘 ─────────");
+			System.out.println("No.\t\t제품명\t\t\t\t수량\t\t가격\t\t맛");
+			for (int i = 0; i < popIndex; i++) {
+				System.out.println((i + 1) + "\t\t" + popcorn[i].getName() + "\t\t\t" + popcorn[i].getAmount() + "\t\t"
+						+ popcorn[i].getPrice() + "\t\t" + popcorn[i].getTaste());
+			}
+			System.out.print("재고를 변경할 제품의 No.를 선택해주세요 >");
+			int sel=scanner.nextInt();
+			System.out.println(popcorn[sel-1].getName()+"제품을 선택하셨습니다.");
+			System.out.println("현재 재고는"+popcorn[sel-1].getAmount()+"입니다.");
+			System.out.print("재고를 몇 개로 수정할지 입력해주세요 > ");
+			int popA=scanner.nextInt();
+			popcorn[sel-1].setAmount(popA);
+			System.out.println(popA+"개로 수정 완료 되었습니다.");
+		}
+		
+		public int deleteDrink(int dIndex, Drink[]drink) {
+			System.out.println("\n───────── 음료 ─────────");
+			System.out.println("No.\t\t제품명\t\t수량\t\t가격");
+			for (int i = 0; i < dIndex; i++) {
+				System.out
+						.println((i + 1) + "\t\t" + drink[i].getName() + "\t\t" + drink[i].getAmount() + "\t\t" + drink[i].getPrice());
+			}
+			System.out.print("선택 >");
+			int choice=scanner.nextInt();
+			System.out.println("삭제 완료");
+			return choice;
+			
+			
+		}
+		public int deleteFood(int fIndex, Food []food) {
+			System.out.println("\n───────── 음식 ─────────");
+			System.out.println("No.\t\t제품명\t\t수량\t\t가격");
+			for (int i = 0; i < fIndex; i++) {
+				System.out
+						.println((i + 1) + "\t\t" + food[i].getName() + "\t\t" + food[i].getAmount() + "\t\t" + food[i].getPrice());
+			}
+			System.out.print("선택 >");
+			int choice=scanner.nextInt();
+			System.out.println("삭제 완료");
+			return choice;
+		}
+		public int deletePopcorn(int popIndex, Popcorn []popcorn) {
+			System.out.println("\n───────── 팝콘 ─────────");
+			System.out.println("No.\t\t제품명\t\t\t\t수량\t\t가격\t\t맛");
+			for (int i = 0; i < popIndex; i++) {
+				System.out.println((i + 1) + "\t\t" + popcorn[i].getName() + "\t\t\t" + popcorn[i].getAmount() + "\t\t"
+						+ popcorn[i].getPrice() + "\t\t" + popcorn[i].getTaste());
+			}
+			System.out.print("선택 >");
+			int choice=scanner.nextInt();
+			System.out.println("삭제 완료");
+			return choice;
+		}
+		public int DeleteThings() {
+			System.out.println("====CGV매점========");
+			System.out.println("======재품 삭제 ======");
+			System.out.println("1. 음료 삭제");
+			System.out.println("2. 음식 삭제");
+			System.out.println("3. 팝콘 삭제");
+			System.out.println("삭제할 제품의 카테고리를 선택해주세요>");
+			int choice=scanner.nextInt();
+			return choice;
+		}
+		public int UpdateThing() {
+			
+			System.out.println("===영화는 역시 CGV ===");
+			System.out.println("====CGV매점========");
+			System.out.println("======재고 수정 ======");
+			System.out.println("1. 음료 수정");
+			System.out.println("2. 음식 수정");
+			System.out.println("3. 팝콘 수정");
+			System.out.print("재고를 변경할 제품의 카테고리를 선태갷주세요 > ");
+			int choice=scanner.nextInt();
+			return choice;
+		}
+		public int insertThing() {
+			System.out.println("-------------------");
+			System.out.println("====CGV매점========");
+			System.out.println("======상품 등록 ======");
+			System.out.println("1. 음료 등록");
+			System.out.println("2. 음식 등록");
+			System.out.println("3. 팝콘 등록");
+			System.out.println("0. 뒤로가기");
+			System.out.print("선택 > ");
+			int choice=scanner.nextInt();
+			return choice;
+		}
 }
