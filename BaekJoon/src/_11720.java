b515312ae69c0f1d66b45b4b4613a2a6b8542484
CHOIHYOGIL <gyrlf2478@naver.com>
2020-09-23 09:45:05 +0900

    학원수강중 코딩연습

---
+++ BaekJoon/src/_11720.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class _11720 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		Scanner scanner=new Scanner(System.in);
+		int sum=0;
+		int n=scanner.nextInt();
+		
+		String line=scanner.next();
+		
+		for(int i=0; i<n; i++) {
+			sum+=line.charAt(i)-'0';
+		}
+
+		System.out.println(sum);
+		
+	}
+
+}
